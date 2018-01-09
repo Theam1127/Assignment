e76b7eadb63ade50138c3befeac1ef3f1a45409d
yongj <yongjin97@hotmail.com>
2018-01-09 17:07:59 +0800

    Changed age to birthday in register activity.

--- app/src/main/java/my/edu/tarc/assignment/DatabaseRequest/RegisterRequest.java
+++ app/src/main/java/my/edu/tarc/assignment/DatabaseRequest/RegisterRequest.java
@@ -10,12 +10,12 @@ public class RegisterRequest extends StringRequest {
     private static String REGISTER_REQUEST_URL = "https://yongjin97.000webhostapp.com/Register.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String username, String password, int age, String email, String gender, Response.Listener<String> listener){
+    public RegisterRequest(String username, String password, String birthday, String email, String gender, Response.Listener<String> listener){
         super(Method.POST, REGISTER_REQUEST_URL, listener, null);
         params = new HashMap<>();
         params.put("username", username);
         params.put("password", password);
-        params.put("age", age + "");
+        params.put("birthday",  birthday + "");
         params.put("email", email);
         params.put("gender", gender);
     }
--- app/src/main/java/my/edu/tarc/assignment/RegisterActivity.java
+++ app/src/main/java/my/edu/tarc/assignment/RegisterActivity.java
@@ -1,11 +1,13 @@
 package my.edu.tarc.assignment;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -18,10 +20,15 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import my.edu.tarc.assignment.DatabaseRequest.RegisterRequest;
 
 public class RegisterActivity extends AppCompatActivity {
     private ProgressDialog pDialog;
+    private Calendar calendar, today;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,22 +37,46 @@ public class RegisterActivity extends AppCompatActivity {
 
         final EditText etRUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etRPassword = (EditText) findViewById(R.id.etPassword);
-        final EditText etRAge = (EditText) findViewById(R.id.etRAge);
+        final EditText etRBirthday = (EditText) findViewById(R.id.etRBirthday);
         final EditText etREmail = (EditText) findViewById(R.id.etREmail);
         Button btnRegister = (Button) findViewById(R.id.btnRegister);
         final RadioGroup rgGender = (RadioGroup) findViewById(R.id.rgGender);
         pDialog = new ProgressDialog(this);
+        calendar = Calendar.getInstance();
+        today = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, day);
+
+                String dateFormat = "MM/dd/yyyy";
+                SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
+                etRBirthday.setText(sdf.format(calendar.getTime()));
+            }
+        };
+
+        etRBirthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(RegisterActivity.this, date, calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = etRUsername.getText().toString();
                 String password = etRPassword.getText().toString();
-                String ageText = etRAge.getText().toString();
+                String birthday = etRBirthday.getText().toString();
                 String email = etREmail.getText().toString();
                 String gender = ((RadioButton) findViewById(rgGender.getCheckedRadioButtonId())).getText().toString();
                 String emailPattern = "[a-zA-z0-a._-]+@[a-z]+\\.+[a-z]+";
                 String passwordPattern = ".{8,}";
+
                 if (username.isEmpty()) {
                     etRUsername.setError(getString(R.string.error_username));
                     return;
@@ -53,8 +84,8 @@ public class RegisterActivity extends AppCompatActivity {
                     etRPassword.setError(getString(R.string.error_password));
                     return;
                 }
-                else if(ageText.isEmpty()) {
-                    etRAge.setError(getString(R.string.error_age));
+                else if(birthday.isEmpty()) {
+                    etRBirthday.setError(getString(R.string.error_birthday));
                     return;
                 } else if (email.isEmpty()) {
                     etREmail.setError(getString(R.string.error_email));
@@ -68,9 +99,10 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
                 else {
-                    int age = Integer.parseInt(ageText);
+                    int age = today.get(Calendar.YEAR) - calendar.get(Calendar.YEAR);
                     if(age<18){
-                        etRAge.setError(getString(R.string.error_age_minimum));
+                        android.app.AlertDialog.Builder age_limit = new android.app.AlertDialog.Builder(RegisterActivity.this);
+                        age_limit.setMessage("You should be minimum 18 years old to use our application!").setNegativeButton("OK",null).create().show();
                         return;
                     }
                     if (!pDialog.isShowing())
@@ -103,7 +135,7 @@ public class RegisterActivity extends AppCompatActivity {
                         }
                     };
 
-                    RegisterRequest registerRequest = new RegisterRequest(username, password, age, email, gender, responseListener);
+                    RegisterRequest registerRequest = new RegisterRequest(username, password, birthday, email, gender, responseListener);
                     RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                     queue.add(registerRequest);
 
@@ -111,5 +143,4 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
-
 }
